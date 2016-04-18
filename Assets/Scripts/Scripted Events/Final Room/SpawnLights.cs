c8e7711f78ad497866cec0ee422d1dd42990a7a8
Nuno Xu <nuno.xu@gmail.com>
2016-04-18 23:24:16 +0100

    Final Build

---
+++ Assets/Scripts/Scripted Events/Final Room/SpawnLights.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Scripted_Events.Final_Room
+{
+    class SpawnLights : MonoBehaviour
+    {
+
+        public Lantern[] lanterns;
+
+
+        void OnTriggerEnter2D(Collider2D e)
+        {
+            foreach(Lantern lant in lanterns)
+            {
+                lant.gameObject.SetActive(true);
+            }
+            this.gameObject.SetActive(false);
+        }
+    }
+}
